5bae11512bc773cb21e999fe8e7e617bd6eca326
angelinarianti01 <angelinarianti23@gmail.com>
2023-03-18 22:44:17 +1100

    Add landing page

--- frontend/landlord/src/views/HomeView.vue
+++ frontend/landlord/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">Welcome to The Perfect Tenant!</h1>
+
+    <br>
+
+    <p class="subtitle">If you are a landlord, you must be looking for the perfect tenant living in your property. By signing up with us, you are able to more than just lease your property. You can find the perfect tenant that suits your needs best</p>
+
+    <a class="button" href="/log-in">Join Us!</a>
   </div>
 </template>
 
@@ -12,7 +17,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
--- frontend/tenant/src/views/HomeView.vue
+++ frontend/tenant/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">Welcome to The Perfect Landlord!</h1>
+
+    <br>
+
+    <p class="subtitle">If you are a tenant, you must be looking for the perfect abode. By signing up with us, you are able to more than just find the perfect house, but also the perfect landlord!</p>
+
+    <a class="button" href="/log-in">Join Us!</a>
   </div>
 </template>
 
